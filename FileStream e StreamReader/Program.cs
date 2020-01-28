8f55e2279097e7c049d28158a9f936554315f71e
Jonatas <modesto.jonatas18@gmail.com>
2020-01-28 18:46:51 -0300

    First commit

---
+++ FileStream e StreamReader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FileStream_e_StreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\File\File1.txt";
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
